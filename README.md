dcfb73ac64c231dd1c790183ecf38e9806a64663
mustafa-alperen-yilmaz <33522591+mustafa-alperen-yilmaz@users.noreply.github.com>
2023-03-06 21:35:47 +0300

    fixing bugs on angular

--- README.md
+++
@@ -1,3 +0,0 @@
-## E-Commerce App
-
-This is an e-commerce application built using Spring Boot and Angular. This app allows users to view, add to cart and purchase products.
---
+++ ecommerceangular/src/app/components/procud-list/procud-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-procud-list',
+  templateUrl: './procud-list.component.html',
+  styleUrls: ['./procud-list.component.css']
+})
+export class ProcudListComponent {
+
+}
