f9a0b0424b5337878abd7a5f88a00fe8662b6cf5
mustafa alperen yılmaz <mayalperen@hotmail.com>
2023-03-23 18:14:42 +0300

    cart item bug fixed

--- .history/ecommerceangular/src/app/services/cart.service_20230323181212.ts
+++ .history/ecommerceangular/src/app/services/cart.service_20230323181212.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {CartItem} from '../common/cart-item';
 import {Subject} from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,51 +10,37 @@ export class CartService {
   cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
-
   constructor() { }
-
   addToCart(theCartItem: CartItem){
-    let alreadyExistsInCart = false;
-    let existingCartItem: CartItem | undefined;
-
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
     if(this.cartItems.length > 0){
-      for(let tempCartItem of this.cartItems){
-        if(tempCartItem.id === theCartItem.id){
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
-    }
-
-    alreadyExistsInCart = (existingCartItem !== undefined);
-
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
+    alreadyExistsInCart =(existingCartItem != undefined);
     if(alreadyExistsInCart){
-      if(existingCartItem){
+      if(existingCartItem !== undefined){
         existingCartItem.quantity++;
       }
     }else{
       this.cartItems.push(theCartItem);
     }
-
     this.computeCartTotals();
+    }
   }
-
   computeCartTotals(){
-    let totalPriceValue = 0;
-    let totalQuantityValue = 0;
-
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
     for(let currentCartItem of this.cartItems){
       totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
       totalQuantityValue += currentCartItem.quantity;
     }
-
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
-
-    this.logCartData(totalPriceValue, totalQuantityValue);
+    this.logCartData(totalPriceValue,totalQuantityValue);
   }
-
   logCartData(totalPriceValue: number , totalQuantityValue: number){
-    console.log('Cart info: Total price = ' + totalPriceValue + ', Total quantity = ' + totalQuantityValue);
+    for(let tempCartItem of this.cartItems){
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+    }
   }
 }
